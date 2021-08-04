a074b4551cd4826c338452b673deff842589747e
FoxImmortal <fox.immortal.code@gmail.com>
2021-08-04 21:13:54 +0300

    fixing the csp

--- project/models.py
+++ project/models.py
@@ -1,92 +1,92 @@
-from datascience.tables import Table
-from project import db
-import requests
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+# from datascience.tables import Table
+# from project import db
+# import requests
+# from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 
-user_resource_fields = {
-    'id': fields.Integer,
-    'name' : fields.String,
-    'password' : fields.Integer,
-    'email' : fields.String,
-    'phone_number' : fields.Integer
-}
+# user_resource_fields = {
+#     'id': fields.Integer,
+#     'name' : fields.String,
+#     'password' : fields.Integer,
+#     'email' : fields.String,
+#     'phone_number' : fields.Integer
+# }
 
-club_resource_fields = {
-    'id': fields.Integer,
-    'clubName' : fields.String,
-    'description' : fields.String,
-    'owner_id' : fields.Integer
-}
-
-
-skill_resource_fields = {
-    'id': fields.Integer,
-    'name' : fields.String,
-    'percentage' : fields.Integer
-}
-
-user_put_args = reqparse.RequestParser()
-user_put_args.add_argument("username", type=str, help="Username of the User is required", required=True)
-user_put_args.add_argument("id", type=int, help="ID of the user", required=True)
-user_put_args.add_argument("skills", type=int, help="Liked of the video", required=True)
-
-BASE = "http://127.0.0.1:5000/"
-
-user_update_args = reqparse.RequestParser()
-user_update_args.add_argument("name", type=str, help="Name of the video is required")
-user_update_args.add_argument("views", type=int, help="Views of the video")
-user_update_args.add_argument("likes", type=int, help="Liked of the video")
+# club_resource_fields = {
+#     'id': fields.Integer,
+#     'clubName' : fields.String,
+#     'description' : fields.String,
+#     'owner_id' : fields.Integer
+# }
 
 
-class User_Club(db.Model, Resource):
-    id = db.Column(db.Integer, primary_key=True)
-    club_guy = db.Column(db.Integer, db.ForeignKey('user.id'))
-    club_skills = db.Column(db.Integer, db.ForeignKey('club.id'))
+# skill_resource_fields = {
+#     'id': fields.Integer,
+#     'name' : fields.String,
+#     'percentage' : fields.Integer
+# }
 
-class Skill(db.Model, Resource) :
-    id = db.Column(db.Integer, primary_key=True)
-    skilled_guy_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    club_skills = db.Column(db.Integer, db.ForeignKey('club.id'), nullable=False)
-    name = db.Column(db.String(30), nullable = False)
-    percentage = db.Column(db.Integer)
+# user_put_args = reqparse.RequestParser()
+# user_put_args.add_argument("username", type=str, help="Username of the User is required", required=True)
+# user_put_args.add_argument("id", type=int, help="ID of the user", required=True)
+# user_put_args.add_argument("skills", type=int, help="Liked of the video", required=True)
 
-class User(db.Model, Resource):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    skills = db.relationship('Skill', backref='skilled_guy', lazy=True)
-    clubs = db.relationship('User_Club', backref='clubsOfMember', lazy=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(30), nullable=False)
-    bio = db.Column(db.String(2000), unique=False, nullable=True)
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-    @marshal_with(user_resource_fields)
-    def post(self, user_id):
-        result = User.query.filter_by(user_id).first()
-        if not result:
-            abort(404, message="Could not find a user with that id...")
-        response = requests.patch(BASE + "User/" + user_id, user_resource_fields)
-        print(response.json())
-        return result
+# BASE = "http://127.0.0.1:5000/"
 
-class Admin_User(db.Model, Resource):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(30), nullable=False)
-    bio = db.Column(db.String(2000), unique=False, nullable=True)
-    def __repr__(self):
-        return f"Admin_User('{self.username}', '{self.email}', '{self.image_file}')"
+# user_update_args = reqparse.RequestParser()
+# user_update_args.add_argument("name", type=str, help="Name of the video is required")
+# user_update_args.add_argument("views", type=int, help="Views of the video")
+# user_update_args.add_argument("likes", type=int, help="Liked of the video")
 
-class Club(db.Model, Resource) :
-    id = db.Column(db.Integer, primary_key=True)
-    requiredSkills = db.relationship('Skill', backref='requiredSkillForTheClub', lazy=True)
-    clubName = db.Column(db.String(120), unique=True, nullable=False)
-    imageFile = db.Column(db.String(20), nullable=False, default='default.jpg')
-    members = db.relationship('User_Club', backref='memberOfClub', lazy=True)
-    description = db.Column(db.String(2000), unique=False, nullable=True)
-    def __repr__(self):
-        return f"Club('{self.clubName}', '{self.imageFile}')"
+
+# class User_Club(db.Model, Resource):
+#     id = db.Column(db.Integer, primary_key=True)
+#     club_guy = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     club_skills = db.Column(db.Integer, db.ForeignKey('club.id'))
+
+# class Skill(db.Model, Resource) :
+#     id = db.Column(db.Integer, primary_key=True)
+#     skilled_guy_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     club_skills = db.Column(db.Integer, db.ForeignKey('club.id'), nullable=False)
+#     name = db.Column(db.String(30), nullable = False)
+#     percentage = db.Column(db.Integer)
+
+# class User(db.Model, Resource):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(30), unique=True, nullable=False)
+#     skills = db.relationship('Skill', backref='skilled_guy', lazy=True)
+#     clubs = db.relationship('User_Club', backref='clubsOfMember', lazy=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(30), nullable=False)
+#     bio = db.Column(db.String(2000), unique=False, nullable=True)
+#     def __repr__(self):
+#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+#     @marshal_with(user_resource_fields)
+#     def post(self, user_id):
+#         result = User.query.filter_by(user_id).first()
+#         if not result:
+#             abort(404, message="Could not find a user with that id...")
+#         response = requests.patch(BASE + "User/" + user_id, user_resource_fields)
+#         print(response.json())
+#         return result
+
+# class Admin_User(db.Model, Resource):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(30), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(30), nullable=False)
+#     bio = db.Column(db.String(2000), unique=False, nullable=True)
+#     def __repr__(self):
+#         return f"Admin_User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# class Club(db.Model, Resource) :
+#     id = db.Column(db.Integer, primary_key=True)
+#     requiredSkills = db.relationship('Skill', backref='requiredSkillForTheClub', lazy=True)
+#     clubName = db.Column(db.String(120), unique=True, nullable=False)
+#     imageFile = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     members = db.relationship('User_Club', backref='memberOfClub', lazy=True)
+#     description = db.Column(db.String(2000), unique=False, nullable=True)
+#     def __repr__(self):
+#         return f"Club('{self.clubName}', '{self.imageFile}')"
     
--- project/routes.py
+++ project/routes.py
@@ -1,37 +1,37 @@
 from flask import render_template, flash, redirect, url_for
 from project import app, bcrypt
-from flask_login import login_user, current_user, logout_user, login_required
-from project.models import User, Admin_User
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from project.forms import LoginForm, RegistrationForm
+# from flask_login import login_user, current_user, logout_user, login_required
+# from project.models import User, Admin_User
+# from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+# from project.forms import LoginForm, RegistrationForm
 
 
-@app.route("/", methods=['GET', 'POST'])
-def ez():
-    return render_template("test.html")
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("index.html")
 
 
 
-@app.route("/api/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        # if user and bcrypt.check_password_hash(user.password, form.password.data):
-        if form.email.data == 'admin@demo.cc':
-            return "YOU HAVE LOGGED IN"
-        else :
-            flash('Login Unsuccessful. Please check E-mail and password', 'danger')
-    return render_template(url_for('ez'))
+# @app.route("/api/login", methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.email.data).first()
+#         # if user and bcrypt.check_password_hash(user.password, form.password.data):
+#         if form.email.data == 'admin@demo.cc':
+#             return "YOU HAVE LOGGED IN"
+#         else :
+#             flash('Login Unsuccessful. Please check E-mail and password', 'danger')
+#     return render_template(url_for('ez'))
 
-@app.route("/api/register", methods=['GET', 'POST'])
-def main():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email = form.email.data, password = hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash(f' Your account has been created {form.username.data}! You now can log in.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+# @app.route("/api/register", methods=['GET', 'POST'])
+# def main():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email = form.email.data, password = hashed_password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash(f' Your account has been created {form.username.data}! You now can log in.', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
